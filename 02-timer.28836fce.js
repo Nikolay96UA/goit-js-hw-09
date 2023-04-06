83cbed063deaa5caeb2b8ff4d563e147309ffb34
Nikolay96UA <115625550+Nikolay96UA@users.noreply.github.com>
2023-04-06 13:50:48 +0000

    Deploying to gh-pages from @ Nikolay96UA/goit-js-hw-09@a7367c7d598be88eb17ef042b2a82387ba557614 🚀

---
+++ 02-timer.28836fce.js
@@ -0,0 +1,2 @@
+document.querySelector("#datatime-picker"),document.querySelector("span[data-days]"),document.querySelector("span[data-hours]"),document.querySelector("span[data-minutes]"),document.querySelector("span[data-seconds]");
+//# sourceMappingURL=02-timer.28836fce.js.map
