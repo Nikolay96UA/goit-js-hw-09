9f0e055d6213fbc3f9582809e573b48a951ee6ac
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-04-06 15:19:10 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.start.addEventListener('click', startColorChange);
+refs.stop.addEventListener('click', stopColorChange);
+
+refs.stop.disabled = true;
+
+function startColorChange() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(timerId);
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
