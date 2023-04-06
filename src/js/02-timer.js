a7367c7d598be88eb17ef042b2a82387ba557614
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-04-06 16:50:23 +0300

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+const refs = {
+  dataTimePicker: document.querySelector('#datatime-picker'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
